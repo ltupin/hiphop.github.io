462d7ee58e45fce034675fab3434bc7485b9302f
minux <minux@minux.fr>
2024-10-01 17:29:54 +0200

    Test with date

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -2,7 +2,7 @@ var store = [{
         "title": "C.Sen - Correspondances",
         "excerpt":"“Pété par l’Pernod, l’Porto, pour moi ça a débuté par un graff / Par la suite, de la peinture dans tous mes paragraphes”     1. Correspondances 9. Ce Soir 2. Bien Ou Bien 10. Grec Frites Style 3. Mon Rêve 11. Un Disque Rayé 4. Le Couloir 12. Voir...","categories": ["Rap","Francais","Post"],
         "tags": ["Rap","Francais","csen","c.sen","c-sen","75018-Beatstreet","75018"],
-        "url": "http://localhost:4000/rap/francais/post/2010/01/01/c-sen.html"
+        "url": "http://localhost:4000/rap/francais/post/2010/10/10/c-sen.html"
       },{
         "title": "Le rap, c'était mieux... Part.1",
         "excerpt":"“Le rap, c’était mieux av… Stop ! C’est tout le temps bon, il suffit de faire le tri.” Cette série d’articles pourra peut-être te permettre de te réconcilier avec le rap francais actuel… Aussi loin que je me souvienne j’ai commencé à vraiment écouter du rap avec une cassette, “Yo...","categories": ["Rap","Article"],
